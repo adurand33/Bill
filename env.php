1b09c2282ce9981b11df0f3cd6002743374177c0
ADurand33 <48013727+adurand33@users.noreply.github.com>
2021-11-15 08:21:02 +0100

    Update env.php
    No pass

--- env.php
+++ env.php
@@ -7,4 +7,4 @@ const CHARSET ='utf8';
 const DB_NAME = 'bill';
 const DB_HOST = 'mysql:host=' . HOST . ';dbname=' . DB_NAME . ';charset=' . CHARSET;
 const DB_USER = 'root';
-const DB_PASS = "appsqladminsecretpass";
+const DB_PASS = "";
